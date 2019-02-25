db4d431b2b81801d64fb259e82a85b0958bcb8bc
Aniruddah <aniruddah.eee@outlook.com>
2019-02-25 13:32:36 +0600

    WebScraping Practice_bikroy.Com

--- src/test/java/io/mavenchu/Scraping/WebScrapingTest.java
+++ src/test/java/io/mavenchu/Scraping/WebScrapingTest.java
@@ -1,19 +1,19 @@
-package io.mavenchu.Scraping;
+ package io.mavenchu.Scraping;
 
 import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlParagraph;
-import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 
 
 public class WebScrapingTest {
 	private static final WebClient CLIENT= getClient();
 	private String Url = "https://bikroy.com/bn/ads/bangladesh/jobs";
+	private String baseUrl = "https://bikroy.com";
+//	private List<Job> jobList = new ArrayList<>();
 	
 	private static WebClient getClient() {
 		WebClient myClient= new WebClient();
@@ -30,26 +30,38 @@ public class WebScrapingTest {
 		HtmlPage page = CLIENT.getPage(Url);
 		CLIENT.waitForBackgroundJavaScript(8*1000);
 		Thread.sleep(3*1000);
-		System.out.println(page.getUrl());
-		System.out.println(page.getTitleText());
-		System.out.println();
+
 		
+		List<HtmlElement> jobList= (List<HtmlElement>) page.getBody().getByXPath("//div[@class='item-content']");
 		
-		List <HtmlAnchor> anchorUrl = (List<HtmlAnchor>) page.getBody().getByXPath("//a[@class='item-title h4']");
-		List <HtmlSpan> spanArea = (List<HtmlSpan>) page.getBody().getByXPath("//span[@class='item-area']");
-        List <HtmlParagraph> spanCompany = (List<HtmlParagraph>) page.getBody().getByXPath("//p[@class='item-meta']");
-		for (HtmlAnchor row :anchorUrl) {
-		System.out.println("Job Url: "+Url+row.getAttribute("href").toString());
-		System.out.println("Job Tilte: "+row.getTextContent());
-//		System.out.println("Company Name: "+row.getTextContent());
-		System.out.println();
-	}
-		
-		for (HtmlParagraph row :spanCompany) {
-			System.out.println("Company Name: "+row.getTextContent());
+		for(int i=0;i<jobList.size();i++) {
+			
+			String jobTitle=jobList.get(i).getHtmlElementsByTagName("a").get(0).getTextContent();
+			String jobUrl=jobList.get(i).getHtmlElementsByTagName("a").get(0).getAttribute("href");
+			String companyName=jobList.get(i).getHtmlElementsByTagName("span").get(0).getTextContent();
+			String jobLocation=jobList.get(i).getHtmlElementsByTagName("p").get(2).getHtmlElementsByTagName("span").get(1).getTextContent();
+			String jobSalary=jobList.get(i).getHtmlElementsByTagName("p").get(1).getHtmlElementsByTagName("strong").get(0).getTextContent();
+			
+			System.out.println("Job No. : "+(i+1));
+			System.out.println("Job title: "+jobTitle);
+			System.out.println("Job Url: "+baseUrl+jobUrl);
+			System.out.println("Company Name: "+companyName);
+			System.out.println("Job Location: "+jobLocation);
+			System.out.println("Job Salary: "+jobSalary);
+			
+			
+			System.out.println();
+			System.out.println();
+			
+			
 		}
+		
+
+	}
 }
-}
+	
+
+
 
 		
 
